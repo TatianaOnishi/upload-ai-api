2a7bb00afe40467f6e7f326f08054fbfb823d707
Tatiana Narumi Matumoto <arumi.matsumoto@gmail.com>
2023-09-14 13:58:24 -0300

    feature: integration with ai

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -18,24 +18,35 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
 
 		const { prompt } = bodySchema.parse(req.body);
 
-        const video = await prisma.video.findUniqueOrThrow({
-            where: {
-                id: videoId,
-            }
-        })
+		const video = await prisma.video.findUniqueOrThrow({
+			where: {
+				id: videoId,
+			},
+		});
 
-        const videoPath = video.path; 
-        const audioReadStream = createReadStream(videoPath);
+		const videoPath = video.path;
+		const audioReadStream = createReadStream(videoPath);
 
-        const response = await openai.audio.transcriptions.create({
-            file: audioReadStream, 
-            model: 'whisper-1',
-            language: 'pt', 
-            response_format: 'json',
-            temperature: 0, 
-            prompt,
-        })
+		const response = await openai.audio.transcriptions.create({
+			file: audioReadStream,
+			model: "whisper-1",
+			language: "pt",
+			response_format: "json",
+			temperature: 0,
+			prompt,
+		});
 
-		return response.text
+		const transcription = response.text;
+
+		await prisma.video.update({
+			where: {
+				id: videoId,
+			},
+			data: {
+				transcription,
+			},
+		});
+
+		return { transcription };
 	});
 }
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,25 @@
 import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify();
 
-app.register(getAllPromptsRoute)
-app.register(uploadVideoRoute)
-app.register(createTranscriptionRoute)
-
-app.listen({
-    port: 3333, 
-}).then(() => {
-    console.log('HTTP Server Running!')
+app.register(fastifyCors, {
+    origin: '*', // Ao colocar em produção, colocar aqui o exato domínio do site que irá utilizar
 })
+
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
+app.register(generateAICompletionRoute);
+
+app
+	.listen({
+		port: 3333,
+	})
+	.then(() => {
+		console.log("HTTP Server Running!");
+	});
